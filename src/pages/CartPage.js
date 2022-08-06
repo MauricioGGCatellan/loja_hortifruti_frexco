b607eb96acb4478cf86fd2587f33e346f2fac081
Maurício Catellan <mauriciocatellan@gmail.com>
2022-08-06 15:10:59 -0300

    Added 'keep buying' button to cart page

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,16 +1,27 @@
 //Cart page view. Shows card items through component CartList (src/components/CartList.js)
 //Cart items are saved via session storage.
 
-import CartList from '../components/CartList'
+import CartList from '../components/CartList';
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+import { useNavigate } from 'react-router-dom';
+
 
 function CartPage() {
+  let navigate = useNavigate();
   
   return (
     <div>
-      <CartList/>
+      <CartList/> 
+      <Grid container justifyContent="space-around">
+        <Grid item> 
+          <Button sx={{mt: 3, backgroundColor: "LightGreen", color: "Green"}} onClick={() => {navigate("/");}}>
+              Keep buying  
+          </Button>
+        </Grid>
+      </Grid>
     </div>
-
   );
-}
-
+} 
+   
 export default CartPage; 
