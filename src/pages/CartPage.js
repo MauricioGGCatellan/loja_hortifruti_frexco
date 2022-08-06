939dc7aa74f21577198b4e0e0ca7166c6bbddf3a
Maurício Catellan <mauriciocatellan@gmail.com>
2022-08-06 15:50:16 -0300

    Added 'reset cart' button

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,15 +1,16 @@
 //Cart page view. Shows card items through component CartList (src/components/CartList.js)
 //Cart items are saved via session storage.
+//'Keep buying' button redirects to products page, and 'Reset your cart' button cleans cart data from session storage
 
 import CartList from '../components/CartList';
 import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
 import { useNavigate } from 'react-router-dom';
-
+ 
 
 function CartPage() {
   let navigate = useNavigate();
-  
+   
   return (
     <div>
       <CartList/> 
@@ -17,6 +18,17 @@ function CartPage() {
         <Grid item> 
           <Button sx={{mt: 3, backgroundColor: "LightGreen", color: "Green"}} onClick={() => {navigate("/");}}>
               Keep buying  
+          </Button>
+        </Grid>
+        <Grid item> 
+          <Button sx={{mt: 3, backgroundColor: "Gainsboro"}} onClick={
+            () => {
+              if(!(sessionStorage.getItem("cart") === null)){
+                sessionStorage.removeItem("cart");
+                window.location.reload();
+              } 
+              }}>
+              Reset your cart 
           </Button>
         </Grid>
       </Grid>
